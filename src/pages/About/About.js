02f378294296c2db47287419076746a1a8c3812e
kumar-modukuri <rajkumar87901311@gmail.com>
2024-11-11 10:20:30 +0530

    final

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,41 +4,48 @@ import "./AboutStyles.css";
 
 const About = () => {
 	return (
-		<div>
+		<>
 			<Navbar />
-			<section className="about">
+			<div className="about-container">
 				<div className="container">
-					<div className="section-title">
-						<h2>About</h2>
+					<div className="about-img">
+						<img src={aboutImg} alt="" />
 					</div>
-
-					<div className="about-content grid">
-						<div className="about-img">
-							<img src={aboutImg} alt="" />
-						</div>
-						<div className="about-text">
-							<h2 className="about-title fs-26 ls-1">About Book Finder</h2>
-							<p className="fs-17">
-								Book Finder is a web application designed to help book
-								enthusiasts discover and explore books effortlessly. Using the
-								OpenLibrary API, Book Finder allows users to search for books by
-								title, author, or keywords and presents detailed information
-								about each book. Users can view book covers, authors,
-								descriptions, publication dates, and subjects—all in one place.
+					<div className="about-text">
+						<h2 className="about-title">About Book Finder</h2>
+						<div className="about-section">
+							<p>
+								<strong>Book Finder</strong> is a web application designed to
+								help book enthusiasts discover and explore books effortlessly.
+								Using the OpenLibrary API, Book Finder allows users to search
+								for books by title, author, or keywords and presents detailed
+								information about each book. Users can view book covers,
+								authors, descriptions, publication dates, and subjects—all in
+								one place.
 							</p>
-							<p className="fs-17">
+							<p>
 								With a clean, user-friendly interface, Book Finder makes it
-								simple to: Discover new books on topics of interest Explore
-								authors and genres in depth View detailed book information,
-								including covers and descriptions Whether you're a student, an
-								avid reader, or someone looking for a quick read, Book Finder is
-								your go-to tool for discovering and learning more about books.
+								simple to:
+							</p>
+							<ul>
+								<li>Discover new books on topics of interest</li>
+								<li>Explore authors and genres in depth</li>
+								<li>
+									View detailed book information, including covers and
+									descriptions
+								</li>
+							</ul>
+							<p>
+								Whether you're a student, an avid reader, or someone looking for
+								a quick read,
+								<strong>Book Finder</strong> is your go-to tool for discovering
+								and learning more about books.
 							</p>
 						</div>
 					</div>
 				</div>
-			</section>
-		</div>
+			</div>
+		</>
 	);
 };
 
